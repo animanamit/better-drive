a7618c781d5a7beade5d02d4ec2b53001e3583b7
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-02-03 16:10:20 +0800

    setup singlestore

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 A simplified clone of Google Clone, focusing on solidifying my understanding of forms, file uploading best practices, as well as more practice for building simple, flat UIs.
 
-## TODO
-
+## TODO (last updated 03/02/2025)
 - [ ] add database and drizzle and data models
 - [ ] move folder state to the URL
 - [ ] add auth via clerk
--- src/env.js
+++ src/env.js
@@ -7,10 +7,16 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+      SINGLESTORE_PASSWORD: z.string(),
+      SINGLESTORE_CONNECTION_STRING: z.string(),
+      SINGLESTORE_HOST: z.string(),
+      SINGLESTORE_PORT: z.string(),
+      SINGLESTORE_USER: z.string(),
+      SINGLESTORE_DB_NAME: z.string()
   },
 
   /**
@@ -29,6 +35,12 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    SINGLESTORE_DB_NAME: process.env.SINGLESTORE_DB_NAME,
+    SINGLESTORE_HOST: process.env.SINGLESTORE_HOST,
+    SINGLESTORE_PORT: process.env.SINGLESTORE_PORT,
+    SINGLESTORE_USER: process.env.SINGLESTORE_USER,
+    SINGLESTORE_PASSWORD: process.env.SINGLESTORE_PASSWORD,
+    SINGLESTORE_CONNECTION_STRING: process.env.SINGLESTORE_CONNECTION_STRING,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
